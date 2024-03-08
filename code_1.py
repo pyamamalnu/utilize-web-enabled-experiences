fc8c160765cc3a94ed16133d6556ba8e657de1a5
pyamamalnu <153632320+pyamamalnu@users.noreply.github.com>
2024-03-08 22:02:24 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,41 @@
+main();
+    return data;
+}
+function generateRandomData() {
+
+
+    }
+}
+    });
+    for (let i = 0; i < 10; i++) {
+function main() {
+    data.forEach(item => {
+
+    const data = [];
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+    public static void main(String[] args) {
+
+        int[] data = new int[10];
+    }
+public class RandomDataGenerator {
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+}
+        }        for (int item : data) {
+        }
+        }
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+
+    }
+
+    public static void main(String[] args) {
+}
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
